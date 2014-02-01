274390ab22706e5cad897ad1f0a5995f08f02dcc
Stefan Schindler <tuxle@estada.ch>
2014-02-01 01:55:05 +0100

    Wrote down the basic Idea of the ProtocolManager.

--- src/org/inchat/protocol/MessageReceiver.java
+++ src/org/inchat/protocol/MessageReceiver.java
@@ -1,43 +1,33 @@
 /*
  * Copyright (C) 2013, 2014 inchat.org
- *
+ * 
  * This file is part of inchat-common.
- *
+ * 
  * inchat-common is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * 
  * inchat-common is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 package org.inchat.protocol;
 
-import java.util.UUID;
+import org.inchat.common.Message;
 
 /**
  *
  */
-public class ProtocolManager {
-    public static final UUID DHT_UUID = new UUID(0,0);
-
-    private static ProtocolManager self = null;
-
+public interface MessageReceiver {
     /**
-     * Load all available transport methods
+     * Handle incomming Messages
+     * @param msg 
      */
-    private ProtocolManager() {
-    }
-
-    public static ProtocolManager getManager() {
-        if (self == null) {
-            self = new ProtocolManager();
-        }
-        return self;
-    }
+    public void onMessage(Message msg);
 }
